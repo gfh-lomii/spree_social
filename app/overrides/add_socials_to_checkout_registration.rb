a85aa2e2e85adb401cbd6d138491ff5fecdaef24
Maximiliano López <maxi.lopez.ramirez@gmail.com>
2021-04-16 14:41:46 -0400

    Bump version

--- app/overrides/add_socials_to_checkout_registration.rb
+++ app/overrides/add_socials_to_checkout_registration.rb
@@ -1,6 +1,13 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_login',
-  name: 'add_socials_login_to_checkout',
-  insert_before: '[data-hook="checkout_login"]',
+  name: 'add_socials_modal_login',
+  insert_before: '[data-hook="modal_user_login"]',
+  partial: 'spree/shared/social'
+)
+
+Deface::Override.new(
+  virtual_path: 'spree/shared/_registration_form',
+  name: 'add_socials_modal_registration',
+  insert_before: '[data-hook="modal_user_registration"]',
   partial: 'spree/shared/social'
 )
